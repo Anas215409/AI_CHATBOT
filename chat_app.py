582889b9613f63946ba24f9868fcf99bc5a10320
Anas215409 <akahmadanas873@gmail.com>
2025-12-29 15:38:47 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Page config
 st.set_page_config(page_title="Gemini Chat", page_icon="🤖")
-st.title("Axis Ai Chatbot 🤖")
+st.title("🤖 Axis Ai Chatbot 🤖")
 
 # Initialize chat history
 if "chat" not in st.session_state:
@@ -70,6 +70,7 @@ if st.sidebar.button("🔄 Reset Conversation"):
     st.experimental_rerun()
 
 st.markdown('**Design and developed by: Anas**')
+
 
 
 
